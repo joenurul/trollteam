1d0b80109fbb2857bc1bd477589fef9f75399ba8
joenurul <nurulzuldamia10@gmail.com>
2021-07-03 16:06:11 +0800

    Add files via upload

--- autobackup.sh
+++ autobackup.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
 
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[ON]${Font_color_suffix}"
-Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
-cek=$(grep -c -E "^# BEGIN_Backup" /etc/crontab)
-if [[ "$cek" = "1" ]]; then
-sts="${Info}"
-else
-sts="${Error}"
-fi
-function start() {
+        function start() {
 email=$(cat /home/email)
 if [[ "$email" = "" ]]; then
 echo "Please enter your email"
@@ -35,7 +22,7 @@ echo " Autobackup Has Been Started"
 echo " Data Will Be Backed Up Automatically at 00:05 GMT +7"
 exit 0
 }
-function stop() {
+       function stop() {
 email=$(cat /home/email)
 sed -i "/^$email/d" /home/email
 sed -i "/^# BEGIN_Backup/,/^# END_Backup/d" /etc/crontab
